e910cd7f025eb8f09cba8f86be27566e163f96b4
Andrei Girnet <g.books2788@gmail.com>
2023-07-26 16:55:29 +0300

    Frontend customization

--- users/views.py
+++ users/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
     template_name = 'users/register.html'
-    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, template_name, {'form': form})
 
