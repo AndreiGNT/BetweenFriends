11a2fdf32a6de72dacc39ea0b24651f09fe1b25c
Andrei Girnet <g.books2788@gmail.com>
2023-07-27 22:51:08 +0300

    creating signals file & auto creating a profile at register

--- BetweenFriends/settings.py
+++ BetweenFriends/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
@@ -135,3 +139,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
