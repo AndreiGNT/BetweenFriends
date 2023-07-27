a37e9ba184e1eaf46332fc5a8030bd5db4ef4da1
Andrei Girnet <g.books2788@gmail.com>
2023-07-27 14:45:00 +0300

    changes registration forms

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 
@@ -7,13 +7,13 @@ def register(request):
     template_name = 'users/register.html'
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, template_name, {'form': form})
 
