db6936fa3372d388553cea0d1235f818aea7b55d
Andrei Girnet <g.books2788@gmail.com>
2023-07-28 17:04:02 +0300

    Create, Update, and Delete Posts

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,60 @@
+from typing import Optional
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
-def home(request):
-    template_name = 'blog/home.html'
-
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, template_name, context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    ordering = ['-date_posted']
+    context_object_name = 'posts'
 
 
-def about(request):
-    template_name = 'blog/about.html'
+class PostDetailView(DetailView):
+    model = Post
     
-    return render(request, template_name)
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class AboutListView(ListView):
+    model = Post
+    template_name = 'blog/about.html' # <app>/<model>_<viewtype>.html
+    ordering = ['-date_posted']
+    context_object_name = 'posts'
