1f9f7fd172f46d93724cf6a8f4788287205b8c1a
Andrei Girnet <g.books2788@gmail.com>
2023-07-29 18:23:37 +0300

    Basic comment section

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,14 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+class Coment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
